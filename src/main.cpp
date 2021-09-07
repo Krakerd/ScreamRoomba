5ea722c373020077da04e3e40d031c3bdd15a6f2
Krakerd <danielzorlej@gmail.com>
2021-09-07 13:57:21 +0200

    intento de hacer funcionar

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ int numeroCanciones = 10;
 
 SoftwareSerial mySoftwareSerial(playerRX, playerTX); //crear un serial de software para los pines del df player(RX,TX)
 DFRobotDFPlayerMini myDFPlayer; //genera un objeto que es el reproductor de sonido
-void printDetail(uint8_t type, int value); //genera una funcion que no hace nada pero es requerida su existencia
+void printDetail(uint8_t type, int value);
 
 void setup() {
   mySoftwareSerial.begin(9600); //serial 9600 para mandar instrucciones al reproductor dfplayer
@@ -29,38 +29,104 @@ void setup() {
     Serial.println(F("1.Please recheck the connection!")); // |
     Serial.println(F("2.Please insert the SD card!"));     // |
 
-    while (true);
+    while (true){delay(0);}
   }
   Serial.println("DFplayer en linea");
 
   myDFPlayer.setTimeOut(500); //colocar el tiempo de espera del reproductor
-  myDFPlayer.volume(20); //volumen del reproductor (max 30)
-  myDFPlayer.EQ(DFPLAYER_EQ_NORMAL); //equalizador
-  myDFPlayer.outputDevice(DFPLAYER_DEVICE_SD); //fuente de los archivos
+  myDFPlayer.volume(10); //volumen del reproductor (max 30)
   
   pinMode(finalA, INPUT); // |---> modo de los pines de entrada
   pinMode(finalV, INPUT); // |
+  myDFPlayer.play(1);
 }
 
 void loop() {
   EstadoA = digitalRead(finalA); // |---> Leer estado de los pines
   EstadoV = digitalRead(finalV); // |
   int randomInt = 0;
-  
+  randomInt = random(1,numeroCanciones+1);
   //comparar con el estado anterior, si es distinto ()
+
+  if(myDFPlayer.available()){
+    printDetail(myDFPlayer.readType(),myDFPlayer.read());
+  }
+
   if(EstadoA != UltEstadoA && EstadoA == HIGH){
-    randomInt = random(1,numeroCanciones+1);
+    
     myDFPlayer.play(randomInt);
     Serial.println("colision detectada");
+    Serial.println(randomInt);
     delay(1000);
   }
   if(EstadoV != UltEstadoV && EstadoV == HIGH){
-    randomInt = random(1,numeroCanciones+1); //elije un numero random [1,num+1)
     myDFPlayer.play(randomInt); //reproduce una cancion elegida por el random 
     Serial.println("colision detectada");
+    Serial.println(randomInt);
     delay(1000);
   }
 
     UltEstadoA = EstadoA;
     UltEstadoV = EstadoV;
 }
+
+void printDetail(uint8_t type, int value){
+switch (type) {
+    case TimeOut:
+      Serial.println(F("Time Out!"));
+      break;
+    case WrongStack:
+      Serial.println(F("Stack Wrong!"));
+      break;
+    case DFPlayerCardInserted:
+      Serial.println(F("Card Inserted!"));
+      break;
+    case DFPlayerCardRemoved:
+      Serial.println(F("Card Removed!"));
+      break;
+    case DFPlayerCardOnline:
+      Serial.println(F("Card Online!"));
+      break;
+    case DFPlayerUSBInserted:
+      Serial.println("USB Inserted!");
+      break;
+    case DFPlayerUSBRemoved:
+      Serial.println("USB Removed!");
+      break;
+    case DFPlayerPlayFinished:
+      Serial.print(F("Number:"));
+      Serial.print(value);
+      Serial.println(F(" Play Finished!"));
+      break;
+    case DFPlayerError:
+      Serial.print(F("DFPlayerError:"));
+      switch (value) {
+        case Busy:
+          Serial.println(F("Card not found"));
+          break;
+        case Sleeping:
+          Serial.println(F("Sleeping"));
+          break;
+        case SerialWrongStack:
+          Serial.println(F("Get Wrong Stack"));
+          break;
+        case CheckSumNotMatch:
+          Serial.println(F("Check Sum Not Match"));
+          break;
+        case FileIndexOut:
+          Serial.println(F("File Index Out of Bound"));
+          break;
+        case FileMismatch:
+          Serial.println(F("Cannot Find File"));
+          break;
+        case Advertise:
+          Serial.println(F("In Advertise"));
+          break;
+        default:
+          break;
+      }
+      break;
+    default:
+      break;
+  }
+} //genera una funcion que no hace nada pero es requerida su existencia
